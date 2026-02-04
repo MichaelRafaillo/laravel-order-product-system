9dfa081be7703738bb8cac544f5d67cece1b48ad
Michael Rafaillo <michael@example.com>
2026-02-04 12:31:16 +0200

    fix: Add missing RouteServiceProvider
    - Create RouteServiceProvider to load api.php routes
    - Register RouteServiceProvider in config/app.php

--- laravel-order-product-system/config/app.php
+++ laravel-order-product-system/config/app.php
@@ -167,6 +167,7 @@ return [
          */
 
         App\Providers\AppServiceProvider::class,
+        App\Providers\RouteServiceProvider::class,
         App\Providers\RepositoryServiceProvider::class,
         App\Providers\EventServiceProvider::class,
 
